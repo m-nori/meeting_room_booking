2c034bf867b0c00d5c10f0cc2ecc17b6d9987013
m_nori <m.norihisa@gmail.com>
2013-01-06 20:31:56 +0900

    トップページ周りを修正

--- routes/root.js
+++ routes/root.js
@@ -2,9 +2,6 @@
 module.exports = function(){
   return {
     index: function(req, res, next) {
-      if(req.session.userID) {
-        res.redirect("/booking");
-      }
       res.render("index");
     }
   };
